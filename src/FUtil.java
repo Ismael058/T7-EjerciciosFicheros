bb75a19bf89d4f499a19cb406a5a4205bede46a0
isnag <ismaelnavarrog08@gmail.com>
2025-05-20 22:19:49 +0200

    Hemos añadido la prueba de la cadena vacia y la excepción

--- src/FUtil.java
+++ src/FUtil.java
@@ -1,10 +1,13 @@
 import java.io.File;
 public class FUtil {
-    public static boolean existe(String filename) {
-    	File file = new File(filename);
-    	if (file.exists()) {
-    		return true;
-    	}
-    	return false;
-    	}
+	public static boolean existe(String filename) throws IllegalArgumentException {
+        if(filename.isBlank()||filename.isEmpty()) {
+            throw new IllegalArgumentException("No válido");
+        }
+        File file = new File(filename);
+        if (file.exists()) {
+            return true;
+        }
+        return false;
     }
+}
--- src/FUtilApp.java
+++ src/FUtilApp.java
@@ -1,11 +1,10 @@
-
 public class FUtilApp {
 
 	public static void main(String[] args) {
-		System.out.println("¿El fichero arch2.txt existe? "+ FUtil.existe("arch2.txt"));
+		System.out.println("¿El fichero arch2.txt existe? " + FUtil.existe("arch2.txt"));
 		System.out.println("");
-		System.out.println("¿El fichero holaMundo.txt existe? "+FUtil.existe("holaMundo.txt"));
-		
+		System.out.println("¿El fichero holaMundo.txt existe? " + FUtil.existe("holaMundo.txt"));
+		System.out.println("");
+		System.out.println("¿El fichero con cadena vacía existe? " + FUtil.existe("")); 
 	}
-
 }
