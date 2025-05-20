43164b9dfa2e28b0a9b602ad249689479a3863ff
isnag <ismaelnavarrog08@gmail.com>
2025-05-20 22:05:06 +0200

    Commit con prueba

---
+++ src/FUtilApp.java
@@ -0,0 +1,11 @@
+
+public class FUtilApp {
+
+	public static void main(String[] args) {
+		System.out.println("¿El fichero arch2.txt existe? "+ FUtil.existe("arch2.txt"));
+		System.out.println("");
+		System.out.println("¿El fichero holaMundo.txt existe? "+FUtil.existe("holaMundo.txt"));
+		
+	}
+
+}
