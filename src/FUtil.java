b21d0554a9d8aa5f04014ece0ff9ef3fd903ca50
isnag <ismaelnavarrog08@gmail.com>
2025-05-20 22:23:56 +0200

    Commit con Documnetacion

--- src/FUtil.java
+++ src/FUtil.java
@@ -1,6 +1,16 @@
+
 import java.io.File;
+
+
 public class FUtil {
-	public static boolean existe(String filename) throws IllegalArgumentException {
+    /**
+     * Verifica si un archivo con el nombre especificado existe en el sistema de archivos.
+     * 
+     * @param filename El nombre del archivo. No puede estar vacío ni en blanco.
+     * @return true si el archivo existe y false si no existe.
+     * @throws IllegalArgumentException Si el nombre del archivo está vacío o tiene solo espacios.
+     */
+    public static boolean existe(String filename) throws IllegalArgumentException {
         if(filename.isBlank()||filename.isEmpty()) {
             throw new IllegalArgumentException("No válido");
         }
